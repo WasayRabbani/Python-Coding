5fd2ebedbee07a23c3e3782f50eed8f0991b4e77
WasayRabbani <wasayrabbani69@gmail.com>
2026-02-03 23:01:11 +0500

    elif

---
+++ 5 Conditional Statements/elif.py
@@ -0,0 +1,9 @@
+age=25
+if age>70 or age <0:
+    print('Enter valid number')
+elif age==18:
+    print('Still can drive')
+elif age>18:
+    print('Can drive')
+else:
+    print('Cannot drive')
---
+++ 5 Conditional Statements/if_else.py
@@ -0,0 +1,35 @@
+
+
+
+age=20
+
+
+if age>18:
+    print('Over age')
+else:
+    print('Under age')
+    
+"""
+Equals: a == b
+Not Equals: a != b
+Less than: a < b
+Less than or equal to: a <= b
+Greater than: a > b
+Greater than or equal to: a >= b
+"""
+
+# Can also have multiple print statements in single if
+if age>18:
+    print('My name is Wasay')
+    print('I am 20 yrs old')
+    print('I am a good boy')   
+    
+    
+# Using Variables in Conditions
+    # Boolean variables can be used directly in if statements without comparison operators.
+    
+logged_in=False
+if logged_in:
+    print('Welcome')
+else:
+    print('False')
