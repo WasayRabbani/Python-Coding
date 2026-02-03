2e905d7ae914797634d28c253661b8cc90c7e29b
WasayRabbani <wasayrabbani69@gmail.com>
2026-02-03 22:36:38 +0500

    .

--- 4 Variables/variables.py
+++ 4 Variables/variables.py
@@ -13,3 +13,30 @@ name=str("Wasay Rabbani")
 num=int('300')
 print(type(num))
 print(num+20)
+
+# Many Values to Multiple Variables
+
+x,y,z='x','y','z'
+print(x)
+
+x,y='hi',20
+print(y)
+
+# One Value to Multiple Variables
+# And you can assign the same value to multiple variables in one line:
+
+
+# Unpack a Collection
+    # Here it means if we have a collection like tuple, list, we can unpack it to different variables 
+
+x,y,z=['haris','ali','saad']
+print(x)
+
+
+"""Output Variables"""
+
+# To output we use print function
+print(x,y,z)
+
+
+"""Global Variables"""
